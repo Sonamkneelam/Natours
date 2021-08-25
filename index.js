9cfd2e122e10cece0fdddecc4db94ae36c04efce
N Sonam <sonamneelam037@gmail.com>
2021-08-25 23:34:39 +0530

    added farm data

---
+++ index.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+const slugify = require("slugify");
